afc62cb25511305c45a9a37c8112796182568710
Annarhysa Albert <annarhysa13@gmail.com>
2022-05-04 22:06:14 +0530

    Create task2.c

---
+++ task2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int arr[6]={1,2,3,4,5,6};
+
+void relocate_print(int i, int array[6], int n)
+{
+	for(int u=6; u>i;u=u-1){
+	 array[u]=array[u-1];
+	 }
+	array[i]=n;
+	for(int i=0; i<=6; i++){
+	printf("%d\n", array[i]);
+	 }
+	return;
+}
+
+int main(){
+	int num;int index;
+	printf("Enter a number: ");
+	scanf("%d", &num);
+	printf("Enter index: ");
+	scanf("%d", &index);
+	relocate_print(index,arr,num);
+  return 0;
+}
